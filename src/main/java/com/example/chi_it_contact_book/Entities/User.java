4f3274070099a23d351e20ef0ad52ed49f2ec08c
YevheniiKyr <jekafekz@gmail.com>
2023-07-06 23:24:52 +0300

    Spring Security Added

--- src/main/java/com/example/chi_it_contact_book/Entities/User.java
+++ src/main/java/com/example/chi_it_contact_book/Entities/User.java
@@ -2,8 +2,12 @@ package com.example.chi_it_contact_book.Entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 @NoArgsConstructor
@@ -15,7 +19,7 @@ import java.util.List;
 @Entity
 @Table(name = "user")
 
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +31,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
     @JsonBackReference
     @OneToMany(
           //  cascade = CascadeType.ALL,
@@ -38,4 +44,30 @@ public class User {
         this.id = id;
         this.username = username;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
